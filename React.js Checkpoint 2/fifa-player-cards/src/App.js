75f3709c486b44b451eafaf2047016f981a819a9
Babatunde Gbolahan <158043186+gbolahan0043@users.noreply.github.com>
2024-06-07 23:20:48 +0100

    Add files via upload

---
+++ React.js Checkpoint 2/fifa-player-cards/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import PlayersList from './components/playerslist';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+};
+
+export default App;
---
+++ React.js Checkpoint 2/fifa-player-cards/src/players.js
@@ -0,0 +1,38 @@
+// src/players.js
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Paris Saint-Germain",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageUrl: "https://b.fssta.com/uploads/application/soccer/headshots/711.vresize.350.350.medium.70.png"
+    },
+    {
+      name: "Cristiano Ronaldo",
+      team: "Manchester United",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 36,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ97p-Dvc-_AnZJpQ0oV9w1QclIaKFisIjspQ&s"
+    },
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 22,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLKgpLZo0JjvGMwTy1pLjDJ-D5_XTexJhjtg&s"
+    },
+    {
+      name: "Neymar Jr",
+      team: "Paris Saint-Germain",
+      nationality: "Brazil",
+      jerseyNumber: 10,
+      age: 29,
+      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrfC5TPxkYkOZGUqB4g7UTuasWX1hs38X5uw&s"
+    }
+  ];
+  
+  export default players;
+  
